1098aadb857eefc2e3e5af85d523355f9fe21ffa
Michael Lee <mhykoly@gmail.com>
2025-02-15 01:08:27 -0800

    Moved to Mantine UI

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    breezy_lib::run()
+    // Necessary due to bug in webkit2gtk on fedora
+    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1"); // alternatively `WEBKIT_DISABLE_COMPOSITING_MODE` if this one is not enough
+    breezy_lib::run();
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import "@mantine/core/styles.css";
+
+import { MantineProvider } from "@mantine/core";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <MantineProvider>
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
 );
